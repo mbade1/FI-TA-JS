818655d613b2e49196853919d0ed3a3afa7ee3f0
Michael Bade <60164494+mbade1@users.noreply.github.com>
2020-04-29 16:00:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -40,6 +40,10 @@ Objectives: Practice writing functions, explain basics of working with strings, 
 
 */
 
+  // "console.log" is a way for the machine to tell you, the programmer something and
+  // "return" is a mechanism for the machine to tell another part of its program something.
+
+
 function shout(string) {
   return string.toUpperCase();
 }
@@ -75,11 +79,47 @@ function sayHiToGrandma(string) {
 /*  FIX THE SCOPE LAB
 
 Obj: explore scope in JS, exercise bug-finding skills related to scope issues
-
-
-
-
 */
+
+var animal = 'dog'
+
+function myAnimal() {
+  // You should not need to modify this function
+  return animal
+}
+
+function yourAnimal() {
+  var animal = 'cat';
+  // The tests expect this function to return `animal` just like the previous function
+  // However, you cannot simply modify the existing variable declared on line 1 in the global scope 
+  // How can we make sure that this function
+  // and the above function both pass?
+  // P.S.: Hard-coding 'cat' below will not work
+  return animal
+}
+
+function add2(n) {
+  const two = 2
+  return n + two
+
+  // Feel free to move things around!
+}
+
+var funkyFunction = function() {
+  return function() {
+    return "FUNKY!"
+  }
+}
+
+// We want 'funkyFunction' on the line below to return a function that returns "FUNKY!" -- how can we accomplish that?
+// NOTE: To pass this final test, you only need to modify the code below this line.
+var theFunk = funkyFunction()
+theFunk = theFunk()
+
+
+
+
+
 
 
 /* BEATLES LOOPS LAB - REVIEW THIS LAB!!!!
